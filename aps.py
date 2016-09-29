cd3c3d4cb5aa7a14b056107c95d76b486ae65e7f
arcticoder <arcticoder@caramail.com>
2016-09-29 00:04:54 -0700

    Paused state

--- aps.py
+++ aps.py
@@ -1,6 +1,6 @@
 import sys, getopt
 from subprocess import call
-from subprocess import Popen,PIPE
+from subprocess import Popen,DEVNULL,PIPE
 
 
 def main(argv):
@@ -14,17 +14,21 @@ def main(argv):
     state = 'unloaded_stopped'
     print('Current state: ' + state)
     while True:
-        command = input('Enter new state (unloaded_stopped,loaded_stopped,loaded_started,exit):')
-        if (command in ['unloaded_stopped','loaded_stopped','loaded_started']):
+        command = input('Enter new state (unloaded_stopped,loaded_stopped,loaded_started,loaded_paused,exit):')
+        if (command in ['unloaded_stopped','loaded_stopped','loaded_started','loaded_paused']):
             state = command
         elif command == 'exit':
             proc.kill()
             sys.exit()
         print('Current state: ' + state)
         if state == 'loaded_started':
-            proc = Popen("mplayer -quiet \"" + infile + "\"", stdout=PIPE, stderr=PIPE)
-        if (state in ['loaded_stopped','unloaded_stopped'] and proc):
+            cmd = ['mplayer', '-slave', '-quiet', infile]
+            proc = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE, universal_newlines=True, bufsize=1)
+        elif (state in ['loaded_stopped','unloaded_stopped'] and proc):
             proc.kill()
+        elif (state == 'loaded_paused' and proc):
+            print('pause', flush=True, file=proc.stdin)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
