b8b653524bc378c000284a589ee2ac111c4df4f8
arcticoder <arcticoder@caramail.com>
2016-10-04 13:17:46 -0700

    imperative to oop

--- aps.py
+++ aps.py
@@ -1,121 +1,138 @@
 import sys, getopt, wave, pyaudio, json, tkinter
 from subprocess import call, Popen, DEVNULL, PIPE
 
-state = None
 
-
-def mouseClicked(event):
-    """
-    Main app logic (loop), traverses SM graph by image region
-
-    :param event:
-    :return:
-    """
-    # TODO: Don't use global variables, refactor into OOP
-    global state, mplayer_proc, root, image_ref, infile
-    wav_chunk_size = 1024
-
-    for edge_key in state_graph[state]['edges']:
-        edge_state = state_graph[state]['edges'][edge_key]
-        if (edge_state['region']['start_x'] <= event.x and
-                    edge_state['region']['start_y'] <= event.y and
-                    edge_state['region']['end_x'] > event.x and
-                    edge_state['region']['end_y'] > event.y):
-            command = edge_key
-
-    # check for transition audio, play if found
-    if ('audio' in state_graph[state]['edges'][command]):
-        audio_path = state_graph[state]['edges'][command]['audio']
-        loading_sound = wave.open(audio_path, "rb")
-        audio_stream = py_audio.open(format=py_audio.get_format_from_width(loading_sound.getsampwidth()),
-                                     channels=loading_sound.getnchannels(),
-                                     rate=loading_sound.getframerate(),
-                                     output=True)
-        audio_data = loading_sound.readframes(wav_chunk_size)
-
-        while len(audio_data) != 0:
-            audio_stream.write(audio_data)
-            audio_data = loading_sound.readframes(wav_chunk_size)
-
-        audio_stream.stop_stream()
-        audio_stream.close()
-        loading_sound.close()
-    previous_state = state
-    state = command
-    if ('img' in state_graph[state]):
-        print('changing to image ' + state_graph[state]['img'])
-        image_ref = tkinter.PhotoImage(file=state_graph[state]['img'])
-        if (image_index):
-            canvas.itemconfig(image_index, image=image_ref)
-        else:
-            canvas.create_image(400, 300, image=image_ref)
-
-    if state == 'loaded_started':
-        if (previous_state == 'loaded_paused'):
-            print('pause', flush=True, file=mplayer_proc.stdin)
-        else:
-            cmd = ['mplayer', '-slave', '-quiet', infile]
-            print('playing ' + infile)
-            mplayer_proc = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE, universal_newlines=True, bufsize=1)
-    elif (state in ['loaded_stopped', 'unloaded_stopped'] and mplayer_proc):
-        mplayer_proc.kill()
-    elif (state == 'loaded_paused' and mplayer_proc):
-        print('pause', flush=True, file=mplayer_proc.stdin)
-
-
-def main(argv):
+class APS:
     """
     Analog Playback Simulator (APS)
     Simulate navigating machine states as it plays back media.
 
+    """
+    state = None
+    mplayer_proc = None
+    state_graph = None
+    root = None
+    py_audio = None
+    image_index = None
+    image_ref = None
+    canvas = None
+    infile = None
+
+    def __init__(self):
+        self.py_audio = pyaudio.PyAudio()
+
+    def init_args(self, argv):
+        opts, args = getopt.getopt(argv, "hi:s:", ["infile=", "machine="])
+        smfile = None
+
+        for opt, arg in opts:
+            if opt == '-h':
+                print(
+                    'Usage:\npython self.py -i <infile> -s <sim_machine_file.json>\ninfile is audio media, sim_machine_file is json')
+                sys.exit()
+            elif opt in ("-i", "--infile"):
+                self.infile = arg
+            elif opt in ("-s", "--machine"):
+                smfile = arg
+
+        if (not self.infile or not smfile):
+            print('Must specify infile and simulated machine file')
+            sys.exit()
+
+        with open(smfile) as json_file:
+            self.state_graph = json.load(json_file)
+
+    def init_canvas(self):
+        # create window
+        self.root = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.root, width=800, height=600)
+        self.canvas.bind("<Button-1>", self.mouse_clicked)
+        self.canvas.pack()
+        self.state = self.state_graph['initial_state']
+
+        if ('img' in self.state_graph[self.state]):
+            self.image_ref = tkinter.PhotoImage(file=self.state_graph[self.state]['img'])
+            self.image_index = self.canvas.create_image(400, 300, image=self.image_ref)
+
+    def main_loop(self):
+        self.root.mainloop()
+
+    def mouse_clicked(self, event):
+        """
+        Main app logic (loop), traverses SM graph by image region
+
+        :param self:
+        :param event:
+        :return:
+        """
+        wav_chunk_size = 1024
+
+        for edge_key in self.state_graph[self.state]['edges']:
+            edge_state = self.state_graph[self.state]['edges'][edge_key]
+            if (edge_state['region']['start_x'] <= event.x and
+                        edge_state['region']['start_y'] <= event.y and
+                        edge_state['region']['end_x'] > event.x and
+                        edge_state['region']['end_y'] > event.y):
+                command = edge_key
+
+        # check for transition audio, play if found
+        if ('audio' in self.state_graph[self.state]['edges'][command]):
+            audio_path = self.state_graph[self.state]['edges'][command]['audio']
+            loading_sound = wave.open(audio_path, "rb")
+            audio_stream = self.py_audio.open(format=self.py_audio.get_format_from_width(loading_sound.getsampwidth()),
+                                              channels=loading_sound.getnchannels(),
+                                              rate=loading_sound.getframerate(),
+                                              output=True)
+            audio_data = loading_sound.readframes(wav_chunk_size)
+
+            while len(audio_data) != 0:
+                audio_stream.write(audio_data)
+                audio_data = loading_sound.readframes(wav_chunk_size)
+
+            audio_stream.stop_stream()
+            audio_stream.close()
+            loading_sound.close()
+        previous_state = self.state
+        self.state = command
+        if ('img' in self.state_graph[self.state]):
+            print('changing to image ' + self.state_graph[self.state]['img'])
+            self.image_ref = tkinter.PhotoImage(file=self.state_graph[self.state]['img'])
+            if (self.image_index):
+                self.canvas.itemconfig(self.image_index, image=self.image_ref)
+            else:
+                self.canvas.create_image(400, 300, image=self.image_ref)
+
+        if self.state == 'loaded_started':
+            if (previous_state == 'loaded_paused'):
+                print('pause', flush=True, file=self.mplayer_proc.stdin)
+            else:
+                cmd = ['mplayer', '-slave', '-quiet', self.infile]
+                print('playing ' + self.infile)
+                self.mplayer_proc = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL, stdin=PIPE, universal_newlines=True,
+                                          bufsize=1)
+        elif (self.state in ['loaded_stopped', 'unloaded_stopped'] and self.mplayer_proc):
+            self.mplayer_proc.kill()
+        elif (self.state == 'loaded_paused' and self.mplayer_proc):
+            print('pause', flush=True, file=self.mplayer_proc.stdin)
+
+    def __del__(self):
+        if self.mplayer_proc:
+            self.mplayer_proc.kill()
+
+        self.py_audio.terminate()
+
+
+def main(argv):
+    """
+    main APS function. instantiates APS class, processes cli params
+
     :param argv:
     :return:
     """
-    global state, state_graph, root, py_audio, image_index, canvas, mplayer_proc, image_ref, infile
-    opts, args = getopt.getopt(argv, "hi:s:", ["infile=", "machine="])
-    infile = None
-    smfile = None
-    mplayer_proc = None
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print(
-                'Usage:\npython aps.py -i <infile> -s <sim_machine_file.json>\ninfile is audio media, sim_machine_file is json')
-            sys.exit()
-        elif opt in ("-i", "--infile"):
-            infile = arg
-        elif opt in ("-s", "--machine"):
-            smfile = arg
-
-    if (not infile or not smfile):
-        print('Must specify infile and simulated machine file')
-        sys.exit()
-
-    with open(smfile) as json_file:
-        state_graph = json.load(json_file)
-
-    py_audio = pyaudio.PyAudio()
-
-    # create window
-    root = tkinter.Tk()
-    canvas = tkinter.Canvas(root, width=800, height=600)
-    canvas.bind("<Button-1>", mouseClicked)
-    canvas.pack()
-    state = state_graph['initial_state']
-    print('Current state: ' + state)
-    image_index = None
-
-    if ('img' in state_graph[state]):
-        image_ref = tkinter.PhotoImage(file=state_graph[state]['img'])
-        image_index = canvas.create_image(400, 300, image=image_ref)
-
-    root.mainloop()
-
-    if mplayer_proc:
-        mplayer_proc.kill()
-
-    py_audio.terminate()
-    sys.exit()
+    aps = APS()
+    aps.init_args(argv)
+    aps.init_canvas()
+    aps.main_loop()
 
 
 if __name__ == "__main__":
